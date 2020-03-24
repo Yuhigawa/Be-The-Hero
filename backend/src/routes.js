838952fee027a1d43e4831d261263fb4e9291832
Pablo Streiski <jose.pablo.com@hotmail.com>
2020-03-24 15:21:15 -0300

    Back-End da Aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
+
+const routes = express.Router()
+
+// Listagem
+routes.get('/ongs', OngController.index);
+routes.get('/incidents', IncidentController.index);
+routes.get('/profiles', ProfileController.index);
+
+// Cadastro
+routes.post('/ongs', OngController.create);
+routes.post('/incidents', IncidentController.create);
+routes.post('/session', SessionController.create);
+
+// Deletar
+routes.delete('/incidents/:id', IncidentController.delete);
+
+module.exports = routes
